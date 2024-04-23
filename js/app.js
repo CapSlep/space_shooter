d7063b0e3f79150b93d9981f88931cf30a6b1ab8
CapSlep <caps0307@gmail.com>
2024-04-23 13:34:12 +0300

    links fixes

--- js/app.js
+++ js/app.js
@@ -26,11 +26,11 @@ const playerMovementSpeed = 25;
 
 //boss
 let bossHP = 4;
-// const bossMovementSpeed = 50;
-// const bossProjectileSpeed = 5;
-// const bossMoveChance = 0.75;
-// const bossShootCooldown = 2;
-// const bossThinkTime = 1000;
+const bossMovementSpeed = 50;
+const bossProjectileSpeed = 5;
+const bossMoveChance = 0.75;
+const bossShootCooldown = 2;
+const bossThinkTime = 1000;
 
 //time
 const timeForGame = 60;
